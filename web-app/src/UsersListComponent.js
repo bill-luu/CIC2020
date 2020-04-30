70f58016eae0f11e6ef398d2badcb79736f60d0f
Paul <paulchenpmc@gmail.com>
2020-04-30 11:29:55 -0600

    Chat components and mockup messages

---
+++ web-app/src/UsersListComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './UsersListComponent.css';
+// import { subscribeToUsersList } from './api'
+
+function User(props) {
+    return (
+        <div style={{paddingLeft: "10px"}}>
+            <span>{props.user}</span>
+        </div>
+    )
+}
+
+class UsersListComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: ['user1', 'medical_staff', 'someguy', 'johnsmith']
+        }
+    }
+
+    componentDidMount() {
+        // subscribeToUsersList((err, users) => {
+        //     this.setState({ users: users })
+        // })
+    }
+
+    render() {
+        return (
+            <div className="usersListComponent">
+                <span style={{paddingLeft: "5px"}}>Users</span>
+                <div className="users users-order">
+                    {/* {this.state.users.map(user => {
+                        return (
+                            <User key={user.clientID} user={user}></User>
+                        )
+                    })} */}
+                    <User key={0} user={this.state.users[0]}></User>
+                    <User key={1} user={this.state.users[1]}></User>
+                    <User key={2} user={this.state.users[2]}></User>
+                    <User key={3} user={this.state.users[3]}></User>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UsersListComponent;
--- web-app/src/chatpage.js
+++ web-app/src/chatpage.js
@@ -2,16 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+// import Alert from '@material-ui/lab/Alert';
+// import Collapse from '@material-ui/core/Collapse';
+
+import MessagesComponent from './MessagesComponent'
+import UsersListComponent from './UsersListComponent'
 
 function Chatpage() {
+  let textProps = {
+    style: {
+      color: 'white'
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Link to='/'><img src={logo} className="App-logo" alt="logo"/></Link>
-        <p>
-          CHATPAGE
-        </p>
       </header>
+      <body>
+        <div className="container">
+          <div className="chatbox">
+            <MessagesComponent username={"someguy"} clientID={"123"}/>
+            <UsersListComponent/>
+            <div className="userInput">
+              <TextField
+                style={{width: "100%"}}
+                label="Message"
+                variant="outlined"
+                InputLabelProps={{
+                  style: {
+                    color: 'white'
+                  }
+                }}
+                InputProps={{
+                  style: {
+                    color: 'white'
+                  }
+                }}
+                // value={"Sample message"}
+                // onKeyPress={(evt) => this.onKeyPress(evt)}
+                // onChange={(evt) => this.onMessageChange(evt)}
+              />
+            </div>
+            {/* <div className="alerts">
+              <Collapse in={this.state.showNameChangeFailed}>
+                <Alert severity="error" onClose= {() => this.handleNameChangeFailedAlertClose()}>
+                  Name Change Failed: {this.state.nameChangeFailedReason}
+                </Alert>
+              </Collapse>
+              <Collapse in={this.state.showColorChangeFailed}>
+                <Alert severity="error" onClose={() => this.handleColorChangeFailedAlertClose()}>
+                  Color Change Failed: Command Format: /nickcolor RRGGBB
+                </Alert>
+              </Collapse>
+              <Collapse in={this.state.invalidSlashCommand}>
+                <Alert severity="error" onClose={() => this.handleInvalidSlashCommandAlertClose()}>
+                  Invalid Slash Command
+                </Alert>
+              </Collapse>
+            </div> */}
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
